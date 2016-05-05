6a286ac10453d307c0a8d9ea8a753fdf3fafd9e1
misho1995 <mnems13@freeuni.edu.ge>
2016-05-05 12:46:32 +0300

    Add maven tests.

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,15 +1,35 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by misho on 5/5/2016.
  */
 public class Tester {
+    @Mock private Operation op;
+
+    private TestClass summer;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        summer = new TestClass(op);
+    }
+
     @Test
     public void testSumSimple(){
-        Operation op = new Operation();
-        TestClass summer = new TestClass(op);
-        assertEquals(summer.getSum(1, 2), 3);
+        Operation oper = new Operation();
+        TestClass sum = new TestClass(oper);
+        assertEquals(sum.getSum(1, 2), 3);
+    }
+
+    @Test
+    public void testUsingMock(){
+        when(op.doOperation(5, 4)).thenReturn(9);
+        assertEquals(summer.getSum(5, 4), 9);
     }
 }
